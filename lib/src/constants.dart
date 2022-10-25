ab5273b11fc4bc58f929df3546dd3666f27d487a
MarkKh792 <ma200@tut.by>
2022-10-25 13:00:29 +0300

    refactor: delete unused connection state check

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,5 +1,6 @@
 const String noDevicesFoundText = 'No bluetooth devices found';
 const String startScanText = 'Start scan';
 const String stopScanText = 'Stop scan';
+const String disconnectText = 'Disconnect';
 
 const int scanTime = 4; // Seconds
--- lib/ui/widgets/device_info.dart
+++ lib/ui/widgets/device_info.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
+import '../../src/constants.dart';
+
 class DeviceInfo extends StatelessWidget {
   const DeviceInfo({super.key, required this.device});
 
@@ -12,39 +14,37 @@ class DeviceInfo extends StatelessWidget {
       stream: device.state,
       initialData: BluetoothDeviceState.connecting,
       builder: (context, snapshot) {
-        VoidCallback? onPressed;
-        String title;
-        Color buttonColor;
-
         if (snapshot.data == BluetoothDeviceState.connected) {
-          title = 'Disconnect';
-          buttonColor = Colors.green;
-          onPressed = () => device.disconnect();
-        } else {
-          buttonColor = Colors.transparent;
-          title = 'Disconnected';
-          onPressed = null;
-        }
-
-        return snapshot.data == BluetoothDeviceState.connected
-            ? Column(
-                children: [
-                  Text('Connected to: ${device.name} (${device.id})'),
-                  Container(
-                    color: buttonColor,
-                    height: 50,
-                    width: 200,
-                    child: TextButton(
-                      child: Text(
-                        title,
-                        style: const TextStyle(fontSize: 25),
-                      ),
-                      onPressed: () => onPressed,
-                    ),
+          return Column(
+            children: [
+              Text('Connected to: ${device.name} (${device.id})'),
+              Container(
+                color: Colors.green,
+                height: 50,
+                width: 200,
+                child: TextButton(
+                  child: const Text(
+                    disconnectText,
+                    style: TextStyle(fontSize: 25),
                   ),
-                ],
-              )
-            : const SizedBox.shrink();
+                  onPressed: () => device.disconnect(),
+                ),
+              ),
+            ],
+          );
+        } else if (snapshot.data != BluetoothDeviceState.disconnected) {
+          return const SizedBox(
+            child: SizedBox(
+              height: 100,
+              width: 100,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          );
+        } else {
+          return const SizedBox.shrink();
+        }
       },
     );
   }
